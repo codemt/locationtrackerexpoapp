27e741953dad002f637aaa2b396c0f815750e598
mithileshtarkar <mithilesh.tarkar@ves.ac.in>
2019-01-01 14:45:46 +0530

    added map component to home container component.

--- src/containers/HomeContainer.js
+++ src/containers/HomeContainer.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import NavBar from '../components/NavBar';
+import Maps from '../components/Maps';
 
 export default class HomeContainer extends Component {
   render() {
     return (
       <React.Fragment>
             <NavBar />
+            <Maps />
       </React.Fragment>
     )
   }
